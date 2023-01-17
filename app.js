417283cf1f64c37fea86fd4f28d486c17a4e6183
33099 <divya030401@gmail.com>
2023-01-18 01:16:04 +0530

    Added ejs,talwind and other packages along with starting server

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+module.exports = app;
